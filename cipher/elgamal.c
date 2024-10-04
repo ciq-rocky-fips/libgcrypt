5c3d83f880897a1ce5f1fb7fa0c08652f3b327eb
Jeremy Allison <jallison@ciq.com>
2024-10-04 11:18:46 -0700

    Add an 'int fips_strict' parameter to the PK verify operations.
    We will use this to allow FIPS disallowed flags when
    used internally via selftest.
    
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- cipher/elgamal.c
+++ cipher/elgamal.c
@@ -1035,7 +1035,7 @@ elg_sign (gcry_sexp_t *r_sig, gcry_sexp_t s_data, gcry_sexp_t keyparms, int fips
 
 
 static gcry_err_code_t
-elg_verify (gcry_sexp_t s_sig, gcry_sexp_t s_data, gcry_sexp_t s_keyparms)
+elg_verify (gcry_sexp_t s_sig, gcry_sexp_t s_data, gcry_sexp_t s_keyparms, int fips_strict)
 {
   gcry_err_code_t rc;
   struct pk_encoding_ctx ctx;
